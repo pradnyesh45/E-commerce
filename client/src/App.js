091f750bda32a9f26f613b93f126ff1181a33a53
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2020-12-31 16:10:16 +0530

    Create React Project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
+
+const App = () => {
+  return Switch;
+};
+
+export default App;
