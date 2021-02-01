3f673212b7272f708ee0366fe61269845f6e76ec
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-02-01 09:52:22 +0530

    NPM Packages to install

--- client/src/pages/auth/ForgotPassword.js
+++ client/src/pages/auth/ForgotPassword.js
@@ -11,7 +11,7 @@ const ForgotPassword = ({ history }) => {
 
   useEffect(() => {
     if (user && user.token) history.push("/");
-  }, []);
+  }, [user]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
